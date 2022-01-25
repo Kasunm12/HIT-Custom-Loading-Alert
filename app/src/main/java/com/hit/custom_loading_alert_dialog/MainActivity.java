a2f155ed24e9667d462d893a8ca81009dd5a8643
KasunW-UoM <kasun.18@itfac.mrt.ac.lk>
2022-01-25 19:40:33 +0530

    add to Loading section

--- app/src/main/java/com/hit/custom_loading_alert_dialog/MainActivity.java
+++ app/src/main/java/com/hit/custom_loading_alert_dialog/MainActivity.java
@@ -3,12 +3,36 @@ package com.hit.custom_loading_alert_dialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login_btn = findViewById(R.id.login_btn);
+        final LoadingDialog loadingDialog  = new LoadingDialog(MainActivity.this);
+
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loadingDialog.startLoadingDialog();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadingDialog.dismissDialog();
+                    }
+                },5000);
+            }
+        });
     }
 }
